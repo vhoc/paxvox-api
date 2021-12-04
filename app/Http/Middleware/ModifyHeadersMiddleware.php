5a69076fdc65faead034ad1994df7b3e22d9c429
vhoc <vhocar@gmail.com>
2021-12-04 11:40:48 -0700

    added headers to middleware

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -19,7 +19,8 @@ class ModifyHeadersMiddleware
 
         $response = $next($request);
         $response->header( 'Access-Control-Allow-Origin', '*' );
-        $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type' );
+        $response->header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        $response->header( 'Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token' );
 
         // Post-Middleware Action
 

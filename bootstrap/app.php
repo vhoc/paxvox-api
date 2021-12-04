c60fa8e98d38b249169b167bc08e29378ed1fc82
vhoc <vhocar@gmail.com>
2021-12-04 12:01:01 -0700

    further fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -78,7 +78,7 @@ $app->middleware([
 
  $app->routeMiddleware([
      'auth' => App\Http\Middleware\Authenticate::class,
-     'cors' => App\Http\Middleware\ModifyHeadersMiddleware::class
+     'cors' => App\Http\Middleware\CorsMiddleware::class
 ]);
 
 /*
@@ -98,7 +98,6 @@ $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Chuckrincon\LumenConfigDiscover\DiscoverServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
-$app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 
 /*
 |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ $router->group(['middleware' => 'auth','prefix' => 'api'], function ($router)
     
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) 
+$router->group(['prefix' => 'api', 'middleware' => 'cors'], function () use ($router) 
 {
    $router->post('register', 'AuthController@register');
    $router->post('login', 'AuthController@login');

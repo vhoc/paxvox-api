9835253f96c1b579a0886796f742217cf949fb33
vhoc <vhocar@gmail.com>
2021-12-08 11:50:37 -0700

    tinkering with Submission Model

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -8,11 +8,14 @@ class Submission extends Model
 {
     protected $table = "submissions";
 
-
     protected $fillable = [
         'cliente_nombre',
         'cliente_email',
         'cliente_telefono',
         'responses->enabled'
+    ];
+
+    protected $casts = [
+        'responses' => 'array'
     ];
 }

c051a6b22a0aef0a4685a96473f0f5782710787e
vhoc <vhocar@gmail.com>
2021-12-14 11:40:47 -0700

    Added Token validation method and route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -90,7 +90,7 @@ class AuthController extends Controller
      * @param Request $request
      * @return Response
      */
-    public function validateToken( Request $requiest )
+    public function validateToken( Request $request )
     {
         $token = '';
 

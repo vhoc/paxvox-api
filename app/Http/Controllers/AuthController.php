4da8dd39f31ab3e4af9f0e2f63f3cad1b6b916e5
vhoc <vhocar@gmail.com>
2021-12-14 11:04:11 -0700

    Added Token validation method and route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -96,7 +96,7 @@ class AuthController extends Controller
 
         try
         {
-            $tokenFetch = $this->jwt->parseToken()->authenticate();
+            $tokenFetch = $this->auth->parseToken()->authenticate();
             if ( $tokenFetch )
             {
                 $token = srt_replace( "Bearer ", "", $request->header('Authorization') );

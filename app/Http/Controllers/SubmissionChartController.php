c9d25242c1021cbf102ca753e83abb29c87bf04f
vhoc <vhocar@gmail.com>
2021-12-20 11:45:55 -0700

    reports for meseros

--- app/Http/Controllers/SubmissionChartController.php
+++ app/Http/Controllers/SubmissionChartController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SubmissonChartController extends Controller
+class SubmissionChartController extends Controller
 {
     
     /**

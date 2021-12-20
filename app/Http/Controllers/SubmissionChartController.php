0eb3033b7444f158c3842189fe80368c682779af
vhoc <vhocar@gmail.com>
2021-12-20 11:46:23 -0700

    reports for meseros

--- app/Http/Controllers/SubmissionChartController.php
+++ app/Http/Controllers/SubmissionChartController.php
@@ -13,8 +13,8 @@ class SubmissionChartController extends Controller
     public function meseros( Request $request )
     {
         $id_location = $request->id_location;
-        $start_date = $request->$start_date;
-        $end_date = $request->$end_date;
+        $start_date = $request->start_date;
+        $end_date = $request->end_date;
 
         return response()->json($request);
     }

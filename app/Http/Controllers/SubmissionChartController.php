95d79a566cbf23fd19d8441ed431a0fdd1e1ed38
vhoc <vhocar@gmail.com>
2021-12-20 14:14:01 -0700

    reports for meseros

--- app/Http/Controllers/SubmissionChartController.php
+++ app/Http/Controllers/SubmissionChartController.php
@@ -30,7 +30,7 @@ class SubmissionChartController extends Controller
         $meseros = [];
         foreach ( $submissions_responses as $response_element => $value )
         {
-            array_push($meseros, $value[0]);
+            array_push($meseros, $value);
         }
 
         return response()->json($meseros);

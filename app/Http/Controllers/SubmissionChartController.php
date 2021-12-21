927b37f4e7f66e7a9b092dca41e1dd1dc2523966
vhoc <vhocar@gmail.com>
2021-12-21 14:16:02 -0700

    finished chart meseros endpoint

--- app/Http/Controllers/SubmissionChartController.php
+++ app/Http/Controllers/SubmissionChartController.php
@@ -56,10 +56,9 @@ class SubmissionChartController extends Controller
         $start_date = $request->start_date;
         $end_date = $request->end_date;
 
-        return $request;
-
         // Select the submissions between the specified dates and belonging to the specified location.
         $submissions_by_date_and_locaton = Submission::whereBetween('created_at', [$start_date, $end_date])->where('id_location', $id_location)->get();
+        return $submissions_by_date_and_locaton;
 
         // Extract only the 'responses' child object from the submissions_by_date_and_locaton parent object.
         $submissions_responses = [];

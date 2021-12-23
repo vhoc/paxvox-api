fcfaf2b3854d78aaa2780f67e667e673f2547163
vhoc <vhocar@gmail.com>
2021-12-23 16:09:14 -0700

    Added location name to login response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
 
         $user = Auth::user($token);
 
-        $user_location = Location::where('id', $user->id_location)->get()->pluck('name');
+        $user_location = Location::where('id', $user->id_location)->get();
 
         //return $this->respondWithToken($token);
         return response()->json([
